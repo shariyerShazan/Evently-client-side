3d0ea5d845aea6b4d161d2ac30c08fd03d8b3c3b
shariyerShazan <shariyershazan1@gmail.com>
2025-05-07 21:09:40 +0600

    final commit

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -25,12 +25,3 @@ export default app
 
 
 
-
-
- // apiKey: "AIzaSyCK8iQMgEdX9hVh_otGVmxIJYDHE0H4w_A",
-  // authDomain: "evently-b4490.firebaseapp.com",
-  // projectId: "evently-b4490",
-  // storageBucket: "evently-b4490.firebasestorage.app",
-  // messagingSenderId: "541968900826",
-  // appId: "1:541968900826:web:27390ef62762d1b18a3be3",
-  // measurementId: "G-F3SR6CMG2L"
